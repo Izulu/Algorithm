7fd3db39d7ebfad162984b34401318abd98cbb4f
Chapell <15221813052m0@sina.cn>
2020-03-19 16:35:23 +0800

    Begin SymbolTable

--- SymbolTable/BST.java
+++ SymbolTable/BST.java
@@ -1,3 +1,4 @@
+import java.util.Queue;
 
 public class BST<Key extends Comparable<Key>, Value> {
     private Node root;
@@ -52,17 +53,141 @@ public class BST<Key extends Comparable<Key>, Value> {
         return false;
     }
 
-    public Iterable<Key> keys(){
-        return new Deque<Key>();
+    public Key min()
+    {
+        return min(root).key;
     }
-//    @Override
-//    public void delete(Object o) {
-//
-//    }
-//
+    private Node min(Node x)
+    {
+        if(x.left == null) return x;
+        return min(x.left);
+    }
 
-//
-//    @Override
+    public Key max()
+    {
+        return max(root).key;
+    }
+    private Node max(Node x)
+    {
+        if(x.right == null) return x;
+        return max(x.right);
+    }
+
+    public Key floor(Key key)
+    {
+        Node x = floor(root, key);
+        if (x==null) return null;
+        return x.key;
+    }
+    private Node floor(Node x,Key key)
+    {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) return x;
+        else if (cmp < 0) return floor(x.left, key);
+        Node t = floor(x.right, key);
+        if (t!=null) return t;
+        else return x;
+    }
+
+    public Key ceiling(Key key)
+    {
+        Node x = ceiling(root, key);
+        if (x==null) return null;
+        return x.key;
+    }
+    private Node ceiling(Node x,Key key)
+    {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) return x;
+        else if (cmp > 0) return ceiling(x.right, key);
+        Node t = ceiling(x.left, key);
+        if (t!=null) return t;
+        else return x;
+    }
+
+    public Key select(int k)
+    {
+        return select(root, k).key;
+    }
+    private Node select(Node x, int k)
+    {
+        if(x==null) return null;
+        int t = size(x.left);
+        if(t>k) return select(x.left, k);
+        else if (t<k) return select(x.right, k-t-1);
+        else return x;
+    }
+
+    public int rank(Key key)
+    {
+        return rank(key, root);
+    }
+    private int rank(Key key, Node x)
+    {
+        if(x==null) return 0;
+        int cmp = key.compareTo(x.key);
+        if(cmp<0) return rank(key, x.left);
+        else if(cmp>0) return rank(key, x.right)+x.left.N+1;
+        else return x.left.N;
+    }
+
+    public void deleteMin()
+    {
+        root = deleteMin(root);
+    }
+    private Node deleteMin(Node x)
+    {
+        if(x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.N = size(x.left)+size(x.right)+1;
+        return x;
+    }
+
+    public void delete(Key key)
+    {
+        root = delete(root,key);
+    }
+    private Node delete(Node x, Key key)
+    {
+        if(x==null) return null;
+        int cmp = key.compareTo(x.key);
+        if(cmp>0) x.right = delete(x.right, key);
+        else if(cmp<0) x.left = delete(x.left, key);
+        else
+        {
+            if(x.left==null) return x.right;
+            if(x.right==null) return x.left;
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right); // The right link to the updated right tree after deleteMin()
+            x.left = t.left;
+        }
+        x.N = size(x.left) + size(x.right) +1;
+        return x;
+    }
+
+    public Iterable<Key> keys()
+    {
+        return keys(min(), max());
+    }
+    private Iterable<Key> keys(Key lo, Key hi)
+    {
+        Deque<Key> deque= new Deque<>();
+        keys(root, deque, lo, hi);
+        return deque;
+    }
+
+    private void keys(Node x, Deque<Key> deque, Key lo, Key hi)
+    {
+        if (x==null) return;
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+        if(cmplo<0) keys(x.left, deque, lo, hi);
+        if(cmphi>0) keys(x.right, deque, lo, hi);
+        if(cmplo <=0 && cmphi >=0) deque.addLast(x.key);
+    }
 //    public boolean isEmpty() {
 //        return false;
 //    }
@@ -71,26 +196,6 @@ public class BST<Key extends Comparable<Key>, Value> {
 //
 //    @Override
 //    public Iterable keys() {
-//        return null;
-//    }
-//
-//    @Override
-//    public Object min() {
-//        return null;
-//    }
-//
-//    @Override
-//    public Object max() {
-//        return null;
-//    }
-//
-//    @Override
-//    public Object floor(Object o) {
-//        return null;
-//    }
-//
-//    @Override
-//    public Object ceiling(Object o) {
 //        return null;
 //    }
 //
